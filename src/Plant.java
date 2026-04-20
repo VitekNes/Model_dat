f4fcaa5a2e86fc75ea2fbf33ef76ab5e8167e41a
VitekNes <vit.nesladek@seznam.cz>
2026-04-20 15:59:57 +0200

    added Plant class with attributes

---
+++ src/Plant.java
@@ -0,0 +1,65 @@
+import java.time.LocalDate;
+
+public class Plant {
+    private String name;
+    private String notes;
+    private LocalDate planted;
+    private LocalDate lastWatering;
+    private int frequencyOfWatering;
+
+    public Plant(String name, String notes, LocalDate planted, LocalDate lastWatering, int frequencyOfWatering){
+        this.name = name;
+        this.notes = notes;
+        this.planted = planted;
+        this.lastWatering = lastWatering;
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+
+    public Plant(String name, int frequencyOfWatering) {
+        this(name, "", LocalDate.now(), LocalDate.now(), frequencyOfWatering);
+    }
+
+    public Plant(String name) {
+        this(name, 7);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDate getPlanted() {
+        return planted;
+    }
+
+    public void setPlanted(LocalDate planted) {
+        this.planted = planted;
+    }
+
+    public LocalDate getLastWatering() {
+        return lastWatering;
+    }
+
+    public void setLastWatering(LocalDate lastWatering) {
+        this.lastWatering = lastWatering;
+    }
+
+    public int getFrequencyOfWatering() {
+        return frequencyOfWatering;
+    }
+
+    public void setFrequencyOfWatering(int frequencyOfWatering) {
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+}
